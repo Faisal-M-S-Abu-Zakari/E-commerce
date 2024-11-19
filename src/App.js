6e4fe08fe485acb034253686ca4ea0ab41af21d5
Faisal <zkryfysl@gmail.com>
2024-11-19 14:45:24 +0200

    design login and register pages , and add routing to them

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./components/utilites/Footer";
 import NavBarLogin from "./components/utilites/NavBarLogin";
 import LoginPage from "./Pages/Auth/LoginPage";
 import { Routes, Route } from "react-router-dom";
+import RegisterPage from "./Pages/Auth/RegisterPage";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
       <Footer />
     </div>
